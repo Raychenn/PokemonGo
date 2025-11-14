7f05f66e8cc3798b271d397254f69fedd17286f7
Boray Chen <r0975929562@gmail.com>
2025-11-14 14:36:43 +0800

    Initial Commit

---
+++ PokemonGoTests/PokemonGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonGoTests.swift
+//  PokemonGoTests
+//
+//  Created by Boray Chen on 2025/11/14.
+//
+
+import Testing
+@testable import PokemonGo
+
+struct PokemonGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
